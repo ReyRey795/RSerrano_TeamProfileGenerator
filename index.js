521e0a7602dc32185804a013860c6633ea332011
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-10-06 22:30:05 -0500

    Added an additional inquirer and fs variable. Began a questions const, just trying to figure out how to make the questions.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// Create questions to be asked for the name, id, 
-// email, role, then each role has it's additional 
-// question.
-
 //Create variables for all classes.
 
 const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
 const Engineer = require('./lib/engineer');
+const Employee = require('./lib/employee');
+
+//node module packages needed
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Create questions to be asked for the name, id, 
+// email, role, then each role has it's additional 
+// question.
+
+
+const questions = 
